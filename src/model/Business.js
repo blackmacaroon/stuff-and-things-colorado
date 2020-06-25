75d335d0f0c535ace0c55a44ff28a35f2f0cb03e
Kayla <6lackmacaroon@gmail.com>
2020-06-25 13:19:53 -0600

    build loadAll and saveAll in Business model

--- src/model/Business.js
+++ src/model/Business.js
@@ -11,30 +11,40 @@ function Business(attributes){
 Business.convertRow2Obj = function (businessRow){ 
     var business = new Business (businessRow);
     return business;
-}
+};
 
 //      LOAD ALL BUSINESSES INSTANCES                                                       
 Business.loadAll = function() {
     var i = 0, key = "", keys=[], businessTableString="", businessTable = {};
     try {
         if(localStorage["businessTable"]){
-            businessTableString = localStorage["businessTable"]
+            businessTableString = localStorage["businessTable"];
         }
-    } catch (err) {
-        alert("Error reading from Local Storage/n" + err)
+    } catch(err) {
+        alert("Error reading from Local Storage\n" + err);
     }
     if(businessTableString){
-        businessTable = JSON.parse(businessTableString)      // "deserialization"
-        keys = Object.keys (businessTable)
-        console.log( keys.length + " businesses loaded")
+        businessTable = JSON.parse(businessTableString);      // "deserialization" = converting the string into a map
+        keys = Object.keys (businessTable);
+        console.log( keys.length + " businesses loaded");
         for(i = 0; i<keys.length; i++){
-            key= keys[i]
-            Business.instance[key] = Business.convertRow2Obj( businessTable[key])
+            key= keys[i];
+            Business.instance[key] = Business.convertRow2Obj( businessTable[key]);
         }
     }
-}
+};
 
 //      SAVE ALL BUSINESS INSTANCES
+Business.saveAll = function() {
+    var bookTableString = "", error=false, numOfBusinesses = Object.keys( Business.instances).length;
+    try {
+        bookTableString = JSON.stringify( Business.instances);      // "serialization" = converting the map into a string
+        localStorage["businessTable"] = businessTableString;
+    } catch(err){
+        alert("Error writing to local storage\n" + err);
+        error = true;
+    }
+};
 
 //      CREATE AND STORE A NEW BUSINESS INSTANCE
 
